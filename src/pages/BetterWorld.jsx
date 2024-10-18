c7265a476bfe6023315cb8f5f6fb574ae1feaa57
Dede Emiko <dede.emiko@gmail.com>
2024-10-17 22:11:10 -0400

    Add mock data

--- src/pages/BetterWorld.jsx
+++ src/pages/BetterWorld.jsx
@@ -6,43 +6,59 @@ import Backarrow from "../assets/icons/arrow_back.svg";
 
 
 
-function BetterWorldPage() {
+function BetterWorld() {
 
-    const [topics, setTopics] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    // const [topics, setTopics] = useState([]);
+    // const [loading, setLoading] = useState(true);
+    // const [error, setError] = useState(null);
 
-
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await axios.get//(API);
-                setTopics(response.data);
-            } catch (error) {
-                setError("Failed to fetch");
-                setLoading(false);
-            }
-        }
-        fetchData();
-    }, []);
+    const [topics, setTopics] = useState([
+        {name: "Mental Health for Everyone" },
+        { name: "Protecting the Environment" },
+        { name: "Safeguarding Data Privacy" },
+        { name: "Accessible Technology for All" }
+    ]);
 
 
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         try {
+    //             const response = await axios.get//(API);
+    //             setTopics(response.data);
+    //         } catch (error) {
+    //             setError("Failed to fetch");
+    //             setLoading(false);
+    //         }
+    //     }
+    //     fetchData();
+    // }, []);
 
-    if (loading) {
-        return <div>One moment please...</div>
-    }
 
-    if (error) {
-        return <div>{error}</div>
-    }
+
+    // if (loading) {
+    //     return <div>One moment please...</div>
+    // }
+
+    // if (error) {
+    //     return <div>{error}</div>
+    // }
         
 
 
     return (
         <div className="better-world">
-            <h2>Better World</h2>
+            <div className="better-world__nav-bar">
+                <Link
+                    to=""
+                    className="better-world__back-arrow"
+                >
+                    <img src={Backarrow} alt="Back arrow" />    
+                </Link>
 
-            <ul>
+                <h2>Better World</h2>
+            </div>
+            
+            <ul className="better-world__topics">
                 {topics.map((topic, index) => (
                     <li key={index}>
                         {topic.name}
