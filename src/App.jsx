6da81068f593ac0d56aa6602d0a9378229af7879
yamuna <yamunajayan@gmail.com>
2024-10-18 11:11:21 -0400

    update list of mental health

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,25 @@ import MentalHealth from "./pages/MentalHealth/MentalHealth.jsx";
 import Community from "./pages/Community/Community.jsx";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <svg tabindex="-1" focusable="false" data-di-rand="1729193652193">
+  return (
+    <BrowserRouter>
+      {/* <svg tabindex="-1" focusable="false" data-di-rand="1729193652193">
                 <use
                     tabindex="-1"
                     focusable="false"
                     xlink:href="#icon-bell-logo"
                 ></use>
-            </svg>
-            <Header />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
+            </svg> */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
 
-                <Route path="/mentalhealth" element={<MentalHealth />} />
+        <Route path="/mentalhealth" element={<MentalHealth />} />
 
-                <Route path="/community" element={<Community />} />
-            </Routes>
-        </BrowserRouter>
-    );
+        <Route path="/community" element={<Community />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
